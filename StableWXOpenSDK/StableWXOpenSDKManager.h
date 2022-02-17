adafbf44c7993437b48f399f465382b7f7c74bf7
Duan Kai <duan.kai@vpclub.cn>
2022-02-17 11:48:49 +0800

    Commit

---
+++ StableWXOpenSDK/StableWXOpenSDKManager.h
@@ -0,0 +1,16 @@
+//
+//  StableWXOpenSDKManager.h
+//  StableWXOpenSDK
+//
+//  Created by 段凯 on 2022/2/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StableWXOpenSDKManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StableWXOpenSDK/StableWXOpenSDKManager.m
@@ -0,0 +1,12 @@
+//
+//  StableWXOpenSDKManager.m
+//  StableWXOpenSDK
+//
+//  Created by 段凯 on 2022/2/17.
+//
+
+#import "StableWXOpenSDKManager.h"
+
+@implementation StableWXOpenSDKManager
+
+@end
